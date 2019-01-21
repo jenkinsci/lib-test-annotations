86d3a1c56e6b339d92021d9853ad5a57f0ede525
Abhyudaya Sharma <sharmaabhyudaya@gmail.com>
2019-01-21 21:30:44 +0530

    Update JavaDoc for SmokeTest.java

--- src/main/java/org/jvnet/hudson/test/SmokeTest.java
+++ src/main/java/org/jvnet/hudson/test/SmokeTest.java
@@ -1,7 +1,15 @@
 package org.jvnet.hudson.test;
 
 /**
- * Interface used as a Category for JUnit tests
+ * Interface used as a Category for classifying a test as a smoke-test.
+ *
+ * <p>
+ * All tests annotated like {@code @Category(SmokeTest.class)} would be run in the {@code smoke-test} suite.
+ * The {@code smoke-test} suite is meant for running unit tests and a number of functional tests.
+ * </p>
+ *
+ * @see <a href="https://github.com/junit-team/junit4/wiki/categories">https://github.com/junit-team/junit4/wiki/categories</a>
+ * @since TODO
  */
 public interface SmokeTest {
 }
